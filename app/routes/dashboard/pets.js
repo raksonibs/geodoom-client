1c67a939c6bc93b2951e16f6e1e92642ce1e713f
Oskar Niburski <oskarniburski@gmail.com>
2016-08-26 16:04:29 -0400

    Pagination via ember-cli-pagination

--- app/routes/dashboard/pets.js
+++ app/routes/dashboard/pets.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
+import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(RouteMixin, {
+  perPage: 3,
   session: Ember.inject.service(),
-  model() {
-    return this.store.findAll('pet');
+  model(params) {    
+    return this.findPaged('pet', params);
   }
 });
